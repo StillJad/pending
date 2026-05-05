4120c73467c27620f6274e44c538ee9349ed71d7
Jad <Norwayjad16@gmail.com>
2026-05-05 21:43:22 +0300

    Export productCategories and ProductCategory types

--- lib/products.ts
+++ lib/products.ts
@@ -1,2 +1,2 @@
-export { products } from "@/data/products";
-export type Product = (typeof import("@/data/products").products)[number];
+export { productCategories, products } from "@/data/products";
+export type { Product, ProductCategory } from "@/data/products";
